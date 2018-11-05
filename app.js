819584a09d35275cdb4de8321c759aece285eeb6
Regan Dufort <regan.t.dufort@gmail.com>
2018-11-05 15:41:50 -0800

    scafolded site and dynamically entered images

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var firstImg = document.getElementById('first');
+var secondImg = document.getElementById('second');
+var thirdImg = document.getElementById('third');
+console.log(firstImg.src);
+
+firstImg.src = './assets/bag.jpg';
+console.log(firstImg.src);
+secondImg.src = './assets/banana.jpg';
+thirdImg.src = './assets/bathroom.jpg';
