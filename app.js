adce69c6eff21171b2517af762a4c5ae6b845e5a
Regan Dufort <regan.t.dufort@gmail.com>
2018-11-06 14:32:16 -0800

    No repeating Images

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var firstImg = document.getElementById('first');
 var secondImg = document.getElementById('second');
 var thirdImg = document.getElementById('third');
 
+
+var previouslyDisplayed = [];
 var allProducts = [];
 var totalClickTracker = 0;
 
@@ -20,26 +22,31 @@ function Product(name, filepath, altText) {
 
 
 function randomImage() {
+
   var firstRandomImage = Math.floor(Math.random() * allProducts.length);
-  console.log("1st random image index: " + firstRandomImage);
-
   var secondRandomImage = Math.floor(Math.random() * allProducts.length);
-  console.log("2nd random image index: " + secondRandomImage);
-
-  while (secondRandomImage === firstRandomImage) {
-    console.log("DUPLICATE 2nd random image index");
-    secondRandomImage = Math.floor(Math.random() * allProducts.length);
-    console.log("2nd random image index: " + secondRandomImage);
-  }
-
   var thirdRandomImage = Math.floor(Math.random() * allProducts.length);
-  console.log("3rd random image index: " + thirdRandomImage);
-  while (thirdRandomImage === secondRandomImage || thirdRandomImage === firstRandomImage) {
-    console.log("DUPLICATE 3rd random image index");
-    thirdRandomImage = Math.floor(Math.random() * allProducts.length);
-    console.log("3rd random image index: " + thirdRandomImage);
-  }
 
+  while (secondRandomImage === firstRandomImage || secondRandomImage === thirdRandomImage || firstRandomImage === thirdRandomImage || previouslyDisplayed.includes(firstRandomImage) || previouslyDisplayed.includes(secondRandomImage) || previouslyDisplayed.includes(thirdRandomImage) ) {
+
+  firstRandomImage = Math.floor(Math.random() * allProducts.length);
+  secondRandomImage = Math.floor(Math.random() * allProducts.length);
+  thirdRandomImage = Math.floor(Math.random() * allProducts.length);
+
+  }
+  
+  previouslyDisplayed[0] = firstRandomImage;
+  previouslyDisplayed[1] = secondRandomImage;
+  previouslyDisplayed[2] = thirdRandomImage;
+  console.log('images previously displayed ' + previouslyDisplayed);
+
+
+
+
+
+
+
+  // function setimages(randindx)
   // Setting image source for all 3 images displayed
   firstImg.src = allProducts[firstRandomImage].filepath;
   secondImg.src = allProducts[secondRandomImage].filepath;
@@ -63,42 +70,42 @@ function randomImage() {
     secondImg.removeEventListener('click', randomImage);
     thirdImg.removeEventListener('click', randomImage);
 // here at the end of the click tracker is where you want to show results
-    displayResults();
-  }
+  //   displayResults();
+   }
 };
 
 function displayResults() {
 
-  //remove all the img elements in main
-
-firstImg = document.getElementById('firstImg');
-firstImg.parentNode.removeChild(firstImg);
-
-secondImg = document.getElementById('secondImg');
-secondImg.parentNode.removeChild(secondImg);
-
-thirdImg = document.getElementById('thirdImg');
-thirdImg.parentNode.removeChild(thirdImg);
-
-  //getelement by id for main
-  var main = getElementById('main');
+  var results = document.getElementById('main-content');
   
 
   //go through all products in for loop
   for (var i = 0; i < allProducts.length; i++) {
 
-    for(allProducts === allProducts[i])
     //create img tag with the src of the filepath at allProducts[i]
-    displayImage = createElementByid
-    //append to main (you got it and set a variable for main above for loop)
+    
+    }
 
-    //create a p tag for the views
-    //append to main (you got it and set a variable for main above for loop)
 
-    //create a p tag for the votes
-    //append to main (you got it and set a variable for main above for loop)    
+
+
+    displayImage = createElementByid('IMG')
+    document.main.appenChild(displayImage)
+
+
+
+    // append to main (you got it and set a variable for main above for loop)
+
+
+
+
+    // create a p tag for the views
+    // append to main (you got it and set a variable for main above for loop)
+
+    // create a p tag for the votes
+    // append to main (you got it and set a variable for main above for loop)    
   }
-};
+
 
 function imageVote(imageSrc) {
 
@@ -132,11 +139,12 @@ new Product('water-can', './assets/water-can.jpg', 'water-can');
 new Product('wine-glass', './assets/wine-glass.jpg', 'wine-glass');
 
 
-firstImg.addEventListener('click', imageVote(firstImg.src));
-secondImg.addEventListener('click', imageVote(secondImg.src));
-thirdImg.addEventListener('click', imageVote(thirdImg.src));
+firstImg.addEventListener('click', randomImage);
+secondImg.addEventListener('click', randomImage);
+thirdImg.addEventListener('click', randomImage);
 
 
 randomImage();
 
 console.log(allProducts);
+
