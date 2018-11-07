642797ff131e7890612ead8a2d5cdb7e74a6ca41
Regan Dufort <regan.t.dufort@gmail.com>
2018-11-06 17:38:42 -0800

    Finished product for Monday

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var totalClickTracker = 0;
 function Product(name, filepath, altText) {
   this.name = name;
   this.filepath = filepath;
-  this.alttext = altText;
+  this.altText = altText;
   this.views = 0;
   this.votes = 0;
 
@@ -27,64 +27,78 @@ function randomImage() {
   var secondRandomImage = Math.floor(Math.random() * allProducts.length);
   var thirdRandomImage = Math.floor(Math.random() * allProducts.length);
 
-  while (secondRandomImage === firstRandomImage || secondRandomImage === thirdRandomImage || firstRandomImage === thirdRandomImage || previouslyDisplayed.includes(firstRandomImage) || previouslyDisplayed.includes(secondRandomImage) || previouslyDisplayed.includes(thirdRandomImage) ) {
+  while (secondRandomImage === firstRandomImage
+    || secondRandomImage === thirdRandomImage
+    || firstRandomImage === thirdRandomImage
+    || previouslyDisplayed.includes(firstRandomImage)
+    || previouslyDisplayed.includes(secondRandomImage)
+    || previouslyDisplayed.includes(thirdRandomImage)) {
 
-  firstRandomImage = Math.floor(Math.random() * allProducts.length);
-  secondRandomImage = Math.floor(Math.random() * allProducts.length);
-  thirdRandomImage = Math.floor(Math.random() * allProducts.length);
+    firstRandomImage = Math.floor(Math.random() * allProducts.length);
+    secondRandomImage = Math.floor(Math.random() * allProducts.length);
+    thirdRandomImage = Math.floor(Math.random() * allProducts.length);
 
   }
-  
+
   previouslyDisplayed[0] = firstRandomImage;
   previouslyDisplayed[1] = secondRandomImage;
-  previouslyDisplayed[2] = thirdRandomImage;
+  previouslyDisplayed[2] = thirdRandomImage
   console.log('images previously displayed ' + previouslyDisplayed);
 
 
 
-  // function setimages(randindx)
+
   // Setting image source for all 3 images displayed
   firstImg.src = allProducts[firstRandomImage].filepath;
   secondImg.src = allProducts[secondRandomImage].filepath;
   thirdImg.src = allProducts[thirdRandomImage].filepath;
 
+  firstImg.alt = allProducts[firstRandomImage].altText;
+  secondImg.alt = allProducts[secondRandomImage].altText;
+  thirdImg.alt = allProducts[thirdRandomImage].altText;
+
+
   //setting the amount of views for each image displayed
   allProducts[firstRandomImage].views++;
-  console.log('views ' + allProducts[firstRandomImage].views);
-
   allProducts[secondRandomImage].views++;
-  console.log('views ' + allProducts[secondRandomImage].views);
-
   allProducts[thirdRandomImage].views++;
-  console.log('views ' + allProducts[thirdRandomImage].views);
 
   totalClickTracker++;
-  console.log(totalClickTracker);
+
 
   if (totalClickTracker === 25) {
     firstImg.removeEventListener('click', randomImage);
     secondImg.removeEventListener('click', randomImage);
     thirdImg.removeEventListener('click', randomImage);
 
-   displayResults();
+    displayResults();
 
-   }
+  }
+};
+
+function handleClick(event) {
+  console.log(event.target.alt);
+
+  randomImage();
+
+
+  for (var i = 0; i < allProducts.length; i++) {
+    if (event.target.alt === allProducts[i].name) {
+      allProducts[i].votes++;
+    }
+  }
 };
 
 function displayResults() {
 
-var results = document.getElementById('results');
+  var results = document.getElementById('results');
 
   for (var i = 0; i < allProducts.length; i++) {
-
     var listViews = document.createElement('li');
-
-
-    listViews.textContent = allProducts[i].views + ' views for the ' + allProducts[i].name;
-
+    listViews.textContent = allProducts[i].views + ' views and ' + allProducts[i].votes + ' votes for the ' + allProducts[i].name;
     results.appendChild(listViews);
-    }
-  };
+  }
+};
 
 
 function imageVote(imageSrc) {
@@ -119,12 +133,9 @@ new Product('water-can', './assets/water-can.jpg', 'water-can');
 new Product('wine-glass', './assets/wine-glass.jpg', 'wine-glass');
 
 
-firstImg.addEventListener('click', randomImage);
-secondImg.addEventListener('click', randomImage);
-thirdImg.addEventListener('click', randomImage);
+firstImg.addEventListener('click', handleClick);
+secondImg.addEventListener('click', handleClick);
+thirdImg.addEventListener('click', handleClick);
 
 
 randomImage();
-
-console.log(allProducts);
-
